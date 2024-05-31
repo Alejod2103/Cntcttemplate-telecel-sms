04399970a4b512cd6b3d353f8f0dafd3c5ef682c
Stefany Guadalupe Arias Núñez <stefany.arias@telecel.com>
2024-05-31 13:23:13 -0600

    segundo commit

---
+++ Responsive Registraion or Sign up Form/contact.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    $to = 'tu@email.com';
+    $subject = 'Nuevo formulario de contacto';
+    $message = "Nombre: $name\n";
+    $message .= "Correo electrónico: $email\n";
+    $message .= "Contraseña: $password\n";
+    $message .= "Confirmar contraseña: $confirm_password\n";
+
+    // Enviar el correo electrónico
+    mail($to, $subject, $message);
+
+    // Redireccionar a una página de confirmación
+    header('Location: confirmation.html');
+    exit;
+}
+?>
